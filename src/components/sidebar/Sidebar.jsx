72db39d7e7ed1e374a57e7138083fdd362270083
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-07-05 17:10:21 +0330

    Modify some files

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,94 +5,176 @@ import { Link as RouterLink } from "react-router-dom";
 function Sidebar() {
   return (
     <Box
+      bg={"blue.100"}
       h={"100vh"}
       borderRight={"1px solid black"}
       py={10}
       position={"sticky"}
       top={0}
       left={0}
-      px={{ base: 2, md: 4 }}
+      px={{ base: 3, md: 6 }}
     >
-      <Flex  gap={10} h={100}>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none" , md:"block"}}>
-            <img src="/logo.png" alt="Instagram logo" />
+      <Flex gap={10} h={100}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          display={{ base: "none", md: "block" }}
+          textDecoration="none"
+        >
+          <img src="/logo.png" alt="Instagram logo" />
         </Link>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"block" , md:"none"}}>
-            <img src="/icons8-instagram-50.png" alt="Instagram logo" />
-        </Link>
-      </Flex>
-
-
-      <Flex  gap={10} h={100}>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none" , md:"block"}}>
-        <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/home_30dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Home logo"/><p>Home</p>
-        </Flex>
-        </Link>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"block" , md:"none"}}>
-            <img src="/home_30dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Home logo" />
+        <Link
+          to={"/"}
+          as={RouterLink}
+          display={{ base: "block", md: "none" }}
+          textDecoration="none"
+        >
+          <img src="/logo2.png" alt="Instagram logo" />
         </Link>
       </Flex>
 
-      <Flex  gap={10} h={100}>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none" , md:"block"}}>
-        <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/search_30dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Search logo"/><p>Search</p>
-        </Flex>
+      <Flex gap={10} h={100}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+          textDecoration="none"
+        >
+          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
+            <img src="/home.png" alt="Home logo" />
+            <p>Home</p>
+          </Flex>
         </Link>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"block" , md:"none"}}>
-            <img src="/search_30dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Search logo" />
-        </Link>
-      </Flex>
-
-
-      <Flex  gap={10} h={100}>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none" , md:"block"}}>
-        <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/favorite_30dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Notifications logo"/><p>Notifications</p>
-        </Flex>
-        </Link>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"block" , md:"none"}}>
-            <img src="/favorite_30dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Notifications logo" />
-        </Link>
-      </Flex>
-
-
-      <Flex  gap={10} h={100}>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none" , md:"block"}}>
-        <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/add_circle_30dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Add a Post logo"/><p>Add a Post</p>
-        </Flex>
-        </Link>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"block" , md:"none"}}>
-            <img src="/add_circle_30dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Add a Post logo" />
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "block", md: "none" }}
+          textDecoration="none"
+        >
+          <img src="/home.png" alt="Home logo" />
         </Link>
       </Flex>
 
-      <Flex mb={20}>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none" , md:"block"}}>
-        <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/profilepic.png" alt="Profile logo" style={{width:"50px" , borderRadius:"50%"}}/><p>Profilee</p>
-        </Flex>
+      <Flex gap={10} h={100}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+        >
+          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
+            <img src="/search.png" alt="Search logo" />
+            <p>Search</p>
+          </Flex>
         </Link>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"block" , md:"none"}}>
-            <img src="/profilepic.png" alt="Profile logo" style={{width:"50px" , borderRadius:"50%"}}/>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "block", md: "none" }}
+        >
+          <img src="/search.png" alt="Search logo" />
         </Link>
       </Flex>
 
-
-        <Flex>
-        <Link to={"/auth"} as={RouterLink} pl={2} display={{base:"none" , md:"block"}}>
-        <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/logout_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Logout logo"/><p>Logout</p>
-        </Flex>
+      <Flex gap={10} h={100}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+        >
+          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
+            <img src="/notifications.png" alt="Notifications logo" />
+            <p>Notifications</p>
+          </Flex>
         </Link>
-        <Link to={"/auth"} as={RouterLink} pl={2} display={{base:"block" , md:"none"}} >
-            <img src="/logout_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.png" alt="Logout logo"/>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "block", md: "none" }}
+        >
+          <img src="/notifications.png" alt="Notifications logo" />
         </Link>
       </Flex>
 
+      <Flex gap={10} h={100}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+        >
+          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
+            <img src="/add.png" alt="Add a Post logo" />
+            <p>Add a Post</p>
+          </Flex>
+        </Link>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "block", md: "none" }}
+        >
+          <img src="/add.png" alt="Add a Post logo" />
+        </Link>
+      </Flex>
 
+      <Flex mb={10}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+        >
+          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
+            <img
+              src="/profile.png"
+              alt="Profile logo"
+              style={{ width: "30px", borderRadius: "50%" }}
+            />
+            <p>Profile</p>
+          </Flex>
+        </Link>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "block", md: "none" }}
+        >
+          <img
+            src="/profile.png"
+            alt="Profile logo"
+            style={{ width: "30px", borderRadius: "50%" }}
+          />
+        </Link>
+      </Flex>
+
+      <Flex>
+        <Link
+          to={"/auth"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+        >
+          <Flex justifyContent={"center"} alignItems={"center"} p={6}>
+            <img src="/logout.png" alt="Logout logo" />
+            <p>Logout</p>
+          </Flex>
+        </Link>
+        <Link
+          to={"/auth"}
+          as={RouterLink}
+          pl={3}
+          display={{ base: "block", md: "none" }}
+          mt={10}
+        >
+          <img src="/logout.png" alt="Logout logo" />
+        </Link>
+      </Flex>
     </Box>
   );
 }
--- src/pages/auth/AuthPage.jsx
+++ src/pages/auth/AuthPage.jsx
@@ -4,11 +4,11 @@ import React from "react";
 
 function AuthPage() {
   return (
-    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4} >
-      <Container maxW={"container.md"} padding={0} >
-        <Flex justifyContent={"center"} alignItems={"center"} gap={10} >
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW={"container.md"} padding={0}>
+        <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
           <Box display={{ base: "none", md: "block" }}>
-            <Image src="/instagram-web-lox-image-2x.png" h={650} w={650} alt="Phone image"></Image>
+            <Image src="/auth2.png" h={650} w={650} alt="Phone image"></Image>
           </Box>
 
           <VStack align={"stretch"}>
