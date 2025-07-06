fb65fc2904c65babf41cf320d8f892d80802da9a
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-07-06 23:09:39 +0330

    Modify some files

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,181 +1,91 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
+import {  Box, Flex, Link, Tooltip } from "@chakra-ui/react";
 import React from "react";
-import { Link as RouterLink } from "react-router-dom";
+import {  Link as RouterLink } from "react-router-dom";
+import { Avatar, AvatarGroup } from "@chakra-ui/react"
 
 function Sidebar() {
   return (
-    <Box
-      bg={"blue.100"}
-      h={"100vh"}
-      borderRight={"1px solid black"}
-      py={10}
-      position={"sticky"}
-      top={0}
-      left={0}
-      px={{ base: 3, md: 6 }}
-    >
-      <Flex gap={10} h={100}>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          display={{ base: "none", md: "block" }}
-          textDecoration="none"
-        >
-          <img src="/logo.png" alt="Instagram logo" />
-        </Link>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          display={{ base: "block", md: "none" }}
-          textDecoration="none"
-        >
-          <img src="/logo2.png" alt="Instagram logo" />
-        </Link>
-      </Flex>
+    <>
+    
+      <Box
+        h={"100vh"}
+        borderRight={"1px solid black"}
+        py={8}
+        position={"sticky"}
+        top={0}
+        left={0}
+        px={{ base: 2, md: 4 }}
+      >
+        <Flex direction={"column"} gap={10} w="full" h={"full"}>
+          <Link to="/" as={RouterLink} pl={2} display={{base:"none",md:"block"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+            <img src="/logo.png"></img>
+          </Link>
+          <Link to="/" as={RouterLink} pl={2} display={{base:"block",md:"none"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+            <img src="/logo2.png"></img>
+          </Link>
 
-      <Flex gap={10} h={100}>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "none", md: "block" }}
-          textDecoration="none"
-        >
-          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/home.png" alt="Home logo" />
-            <p>Home</p>
-          </Flex>
-        </Link>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "block", md: "none" }}
-          textDecoration="none"
-        >
-          <img src="/home.png" alt="Home logo" />
-        </Link>
-      </Flex>
 
-      <Flex gap={10} h={100}>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "none", md: "block" }}
-        >
-          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/search.png" alt="Search logo" />
-            <p>Search</p>
-          </Flex>
-        </Link>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "block", md: "none" }}
-        >
-          <img src="/search.png" alt="Search logo" />
-        </Link>
-      </Flex>
 
-      <Flex gap={10} h={100}>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "none", md: "block" }}
-        >
-          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/notifications.png" alt="Notifications logo" />
-            <p>Notifications</p>
-          </Flex>
-        </Link>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "block", md: "none" }}
-        >
-          <img src="/notifications.png" alt="Notifications logo" />
-        </Link>
-      </Flex>
+          <Link to="/" as={RouterLink} pl={2} display={{base:"none",md:"block"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+          <img src="/home.png"></img><p>Home</p>
+          </Link>
+          <Link to="/" as={RouterLink} pl={2} display={{base:"block",md:"none"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+            <img src="/home.png"></img>
+          </Link>
 
-      <Flex gap={10} h={100}>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "none", md: "block" }}
-        >
-          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img src="/add.png" alt="Add a Post logo" />
-            <p>Add a Post</p>
-          </Flex>
-        </Link>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "block", md: "none" }}
-        >
-          <img src="/add.png" alt="Add a Post logo" />
-        </Link>
-      </Flex>
 
-      <Flex mb={10}>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "none", md: "block" }}
-        >
-          <Flex justifyContent={"center"} alignItems={"center"} p={5}>
-            <img
-              src="/profile.png"
-              alt="Profile logo"
-              style={{ width: "30px", borderRadius: "50%" }}
-            />
+
+          <Link to="/" as={RouterLink} pl={2} display={{base:"none",md:"block"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+          <img src="/search.png"></img><p>Search</p>
+          </Link>
+          <Link to="/" as={RouterLink} pl={2} display={{base:"block",md:"none"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+            <img src="/search.png"></img>
+          </Link>
+
+
+          <Link to="/" as={RouterLink} pl={2} display={{base:"none",md:"block"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+          <img src="/notifications.png"></img><p>Notifications</p>
+          </Link>
+          <Link to="/" as={RouterLink} pl={2} display={{base:"block",md:"none"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+            <img src="/notifications.png"></img>
+          </Link>
+
+
+          <Link to="/" as={RouterLink} pl={2} display={{base:"none",md:"block"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+          <img src="/add.png"></img><p>Add a post</p>
+          </Link>
+          <Link to="/" as={RouterLink} pl={2} display={{base:"block",md:"none"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+            <img src="/add.png"></img>
+          </Link>
+
+
+          <Link to="/" as={RouterLink} pl={2} display={{base:"none",md:"block"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+            <Avatar.Root>
+              <Avatar.Fallback name="Mohammad Ebadi" />
+              <Avatar.Image src="/profile.png"/>
+            </Avatar.Root>
             <p>Profile</p>
-          </Flex>
-        </Link>
-        <Link
-          to={"/"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "block", md: "none" }}
-        >
-          <img
-            src="/profile.png"
-            alt="Profile logo"
-            style={{ width: "30px", borderRadius: "50%" }}
-          />
-        </Link>
-      </Flex>
+         
+          </Link>
+          <Link to="/" as={RouterLink} pl={2} display={{base:"block",md:"none"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+            <Avatar.Root>
+              <Avatar.Fallback name="Mohammad Ebadi" />
+              <Avatar.Image src="/profile.png"/>
+            </Avatar.Root>
+          </Link>
 
-      <Flex>
-        <Link
-          to={"/auth"}
-          as={RouterLink}
-          pl={2}
-          display={{ base: "none", md: "block" }}
-        >
-          <Flex justifyContent={"center"} alignItems={"center"} p={6}>
-            <img src="/logout.png" alt="Logout logo" />
-            <p>Logout</p>
-          </Flex>
-        </Link>
-        <Link
-          to={"/auth"}
-          as={RouterLink}
-          pl={3}
-          display={{ base: "block", md: "none" }}
-          mt={10}
-        >
-          <img src="/logout.png" alt="Logout logo" />
-        </Link>
-      </Flex>
-    </Box>
+
+          <Link to="/auth" as={RouterLink} pl={2} display={{base:"none",md:"block"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+          <img src="/logout.png"></img><p>Logout</p>
+          </Link>
+          <Link to="/auth" as={RouterLink} pl={2} display={{base:"block",md:"none"}} _hover={{bg:"blackAlpha.500" , borderRadius:10}}>
+            <img src="/logout.png"></img>
+          </Link>
+
+      
+        </Flex>
+      </Box>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "./components/ui/provider.jsx";
 // import { ChakraProvider } from "@chakra-ui/react";
 // import { Provider } from "@/components/ui/provider"
+import { Avatar, AvatarGroup } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider>
-        <App />
+        <AvatarGroup>
+          <App />
+        </AvatarGroup>
       </Provider>
     </BrowserRouter>
   </StrictMode>
