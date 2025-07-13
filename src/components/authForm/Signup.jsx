1340cd42167446b60963e468168c0d64cf3d5585
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-07-13 19:10:18 +0330

    Modify some files

--- src/components/authForm/Signup.jsx
+++ src/components/authForm/Signup.jsx
@@ -2,15 +2,16 @@ import { Button, Input } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/config/firebase";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
-  // const navigate=useNavigate("")
+  const navigate=useNavigate("")
   const handleSignup = async () => {
     try {
       await createUserWithEmailAndPassword(auth, inputs.email, inputs.password);
       alert("OK")
       console.log("Clicked")
+      navigate("/")
     } catch (error) {
       alert(error.message)
 
