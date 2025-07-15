2fc0b963e7118950433f06288574584b172c05d2
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-07-15 20:24:30 +0330

    Modify some files

--- src/components/suggestedUsers/SuggestedHeader.jsx
+++ src/components/suggestedUsers/SuggestedHeader.jsx
@@ -1,53 +1,56 @@
 import { Avatar, Box, Flex, Link, Text } from "@chakra-ui/react";
 import React from "react";
-import { For, HStack } from "@chakra-ui/react";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
-import { auth ,firestore} from "@/config/firebase";
+import { auth, firestore } from "@/config/firebase";
 import { signOut } from "firebase/auth";
-import { doc,getDoc } from "firebase/firestore";
-import { useEffect,useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 function SuggestedHeader() {
+  const navigate = useNavigate();
+  const [authUser, setAuthUser] = useState(null);
 
-  const navigate =  useNavigate()
-  const [authUser,setAuthUser]= useState(null)
-
-  useEffect(()=>{
-     const fetchUserData = async()=>{
-      const currentUser= auth.currentUser;
-      if(currentUser){
-        const userDocRef = doc(firestore, "users",currentUser.uid)
-        const userSnap = await getDoc(userDocRef)
-        if(userSnap.exists()){
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        const userDocRef = doc(firestore, "users", currentUser.uid);
+        const userSnap = await getDoc(userDocRef);
+        if (userSnap.exists()) {
           setAuthUser(userSnap.data());
         }
       }
-     }
-     fetchUserData()
-  },[])
-   
-  const handleLogout = async ()=>{
-    
+    };
+    fetchUserData();
+  }, []);
+
+  const handleLogout = async () => {
     try {
-      await signOut(auth)
+      await signOut(auth);
       localStorage.removeItem("user-Info");
-      navigate("/auth")
-      
+      navigate("/auth");
     } catch (error) {
-      console.log(error)
-      
+      console.log(error);
     }
-  }
+  };
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
       <Flex alignItems={"center"} gap={2}>
-        <Avatar.Root size={"sm"}>
-          <Avatar.Fallback name="Profile" />
-          <Avatar.Image src={authUser.profilePicURL} />
-        </Avatar.Root>
-        {/* show user name here */}
-        <Text fontSize={12} fontWeight={"bold"}>
-          {authUser.userName}
-        </Text>
+        {authUser ? (
+          <>
+            <Avatar.Root size={"sm"}>
+              <Avatar.Fallback name="Profile" />
+              <Avatar.Image src={authUser.profilePicURL || ""} />
+            </Avatar.Root>
+            {/* show user name here */}
+            <Text fontSize={12} fontWeight={"bold"}>
+              {authUser.userName}
+            </Text>
+          </>
+        ) : (
+          <Text fontSize={12} color="red.500">
+            Loading ...
+          </Text>
+        )}
       </Flex>
       <Link
         as={RouterLink}
@@ -56,7 +59,9 @@ function SuggestedHeader() {
         textDecoration={"none"}
         _hover={{ color: "red.500", transition: "0.2s" }}
         fontWeight="bold"
-        onClick={()=>{handleLogout()}}
+        onClick={() => {
+          handleLogout();
+        }}
       >
         Log out
       </Link>
