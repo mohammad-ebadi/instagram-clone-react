0bae54867924163106b2405f50e18404336a3f3f
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-07-15 18:46:23 +0330

    Modify some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import AuthPage from "./pages/auth/AuthPage";
 import PageLayout from "./layouts/pageLayout/PageLayout";
--- src/components/suggestedUsers/SuggestedHeader.jsx
+++ src/components/suggestedUsers/SuggestedHeader.jsx
@@ -1,8 +1,24 @@
 import { Avatar, Box, Flex, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import { For, HStack } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { auth } from "@/config/firebase";
+import { signOut } from "firebase/auth";
 function SuggestedHeader() {
+
+  const navigate=useNavigate()
+  const handleLogout = async ()=>{
+    
+    try {
+      await signOut(auth)
+      localStorage.removeItem("user-Info");
+      navigate("/auth")
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
       <Flex alignItems={"center"} gap={2}>
@@ -21,6 +37,7 @@ function SuggestedHeader() {
         textDecoration={"none"}
         _hover={{ color: "red.500", transition: "0.2s" }}
         fontWeight="bold"
+        onClick={()=>{handleLogout()}}
       >
         Log out
       </Link>
