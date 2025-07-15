57dc8789f6d8f4a38f66c8dca8082f6199d5721d
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-07-15 18:37:19 +0330

    Modify some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import AuthPage from "./pages/auth/AuthPage";
 import PageLayout from "./layouts/pageLayout/PageLayout";
 import ProfilePage from "./pages/profilePage/ProfilePage";
 // import WrongRoute from "./pages/404/wrongRoute";
+import { auth } from "./config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const [user,setUser]=useState(null)
+
+
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
+      setUser(currentUser)
+    })
+
+    return()=> unsubscribe()
+  },[])
   return (
     <PageLayout>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/auth" element={<AuthPage></AuthPage>}></Route>
-        <Route path="/:username" element={<ProfilePage></ProfilePage>}></Route>
+        <Route path="/" element={user ? <Home></Home> : <Navigate to="/auth" replace></Navigate>}></Route>
+        <Route path="/auth" element={!user ? <AuthPage></AuthPage>: <Navigate to="/" replace></Navigate>}></Route>
+        <Route path="/:username" element={user ? <ProfilePage></ProfilePage>: <Navigate to="/auth" replace></Navigate>}></Route>
         {/* <Route path="*" element={<WrongRoute></WrongRoute>}></Route> */}
       </Routes>
     </PageLayout>
