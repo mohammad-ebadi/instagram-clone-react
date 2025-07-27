60e39cac56b277028b754f190120564f9da6c61c
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-07-27 21:00:00 +0330

    Modify some files

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "./components/ui/provider.jsx";
-// import { ChakraProvider } from "@chakra-ui/react";
-// import { Provider } from "@/components/ui/provider"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
