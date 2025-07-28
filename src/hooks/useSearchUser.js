c563594095a63fdc195e47358953ef92b8477456
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-07-28 15:34:08 +0330

    Modify some files

---
+++ src/hooks/useSearchUser.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore } from "../config/firebase.jsx";
+
+const useSearchUser = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [user, setUser] = useState(null);
+
+	const getUserProfile = async (username) => {
+		setIsLoading(true);
+		setUser(null);
+		try {
+			const q = query(collection(firestore, "users"), where("username", "==", username));
+
+			const querySnapshot = await getDocs(q);
+			if (querySnapshot.empty) return
+
+			querySnapshot.forEach((doc) => {
+				setUser(doc.data());
+			});
+		} catch (error) {
+			console.log(error)
+			setUser(null);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return { isLoading, getUserProfile, user, setUser };
+};
+
+export default useSearchUser;
