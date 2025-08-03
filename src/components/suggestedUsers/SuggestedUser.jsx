bd837887b243ba8a078d6d3a62abb3a58e57e486
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-08-03 20:47:05 +0330

    Modify some files

--- src/components/suggestedUsers/SuggestedUser.jsx
+++ src/components/suggestedUsers/SuggestedUser.jsx
@@ -1,46 +1,4 @@
 
-
-// import { Avatar, VStack, Text, Flex, Button } from "@chakra-ui/react";
-
-// function SuggestedUser({
-//   userName,
-//   avatar,
-//   followersCount,
-//   fullName,
-//   isFollowing,
-//   onToggleFollow,
-// }) {
-//   return (
-//     <Flex w="full" justify="space-between" align="center" py={2}>
-//       <Flex align="center" gap={3}>
-//         {/* <Avatar size="sm" name={fullName} src={avatar} /> */}
-//         <Avatar.Root size={"sm"}>
-//           <Avatar.Fallback name={fullName} />
-//           <Avatar.Image src={avatar} />
-//         </Avatar.Root>
-//         <VStack spacing={0} align="start">
-//           <Text fontSize="sm" fontWeight="bold">
-//             {userName}
-//           </Text>
-//           <Text fontSize="xs" color="gray.500">
-//             {followersCount} followers
-//           </Text>
-//         </VStack>
-//       </Flex>
-//       <Button
-//         size="sm"
-//         colorScheme={isFollowing ? "gray" : "blue"}
-//         onClick={onToggleFollow}
-//       >
-//         {isFollowing ? "Unfollow" : "Follow"}
-//       </Button>
-//     </Flex>
-//   );
-// }
-
-// export default SuggestedUser;
-
-
 import { Avatar, VStack, Text, Flex, Button } from "@chakra-ui/react";
 
 function SuggestedUser({ userName, avatar, followersCount, fullName, isFollowing, onToggleFollow }) {
--- src/components/suggestedUsers/SuggestedUsers.jsx
+++ src/components/suggestedUsers/SuggestedUsers.jsx
@@ -1,103 +1,4 @@
 
-
-// import React, { useEffect, useState } from "react";
-// import { VStack, Box, Text } from "@chakra-ui/react";
-// import SuggestedUser from "./SuggestedUser.jsx";
-// import { collection, getDocs } from "firebase/firestore";
-// import { getAuth } from "firebase/auth";
-// import { firestore } from "../../config/firebase.jsx";
-// import SuggestedHeader from "./SuggestedHeader.jsx";
-
-// function SuggestedUsers() {
-//   const [users, setUsers] = useState([]);
-//   const currentUser = getAuth().currentUser;
-
-//   useEffect(() => {
-//     const fetchUsers = async () => {
-//       try {
-//         const querySnapshot = await getDocs(collection(firestore, "users"));
-//         const usersData = querySnapshot.docs
-//           .map((doc) => ({
-//             id: doc.id,
-//             ...doc.data(),
-//           }))
-//           .filter((user) => user.uid !== currentUser?.uid)
-//           .slice(0, 10);
-//         setUsers(usersData);
-//       } catch (error) {
-//         console.error("خطا در دریافت کاربران:", error);
-//       }
-//     };
-//     fetchUsers();
-//   }, [currentUser]);
-
-//   // وضعیت فالو/آنفالو را در یک آبجکت نگه داریم: { uid: true/false }
-//   const [followState, setFollowState] = useState({});
-
-//   useEffect(() => {
-//     if (!currentUser) return;
-//     // مقداردهی اولیه followState بر اساس اینکه uid در followers کاربر است یا نه
-//     const initialFollowState = {};
-//     users.forEach((user) => {
-//       initialFollowState[user.uid] = user.followers.includes(currentUser.uid);
-//     });
-//     setFollowState(initialFollowState);
-//   }, [users, currentUser]);
-
-//   // تابع تغییر وضعیت فالو/آنفالو
-//   const toggleFollow = (uid) => {
-//     setFollowState((prev) => {
-//       const isFollowing = prev[uid];
-//       // آپدیت آرایه کاربران برای تعداد فالوورها
-//       setUsers((prevUsers) =>
-//         prevUsers.map((user) => {
-//           if (user.uid === uid) {
-//             let updatedFollowers = [...user.followers];
-//             if (isFollowing) {
-//               // آنفالو - حذف currentUser.uid از followers
-//               updatedFollowers = updatedFollowers.filter(
-//                 (f) => f !== currentUser.uid
-//               );
-//             } else {
-//               // فالو - اضافه کردن currentUser.uid به followers
-//               updatedFollowers.push(currentUser.uid);
-//             }
-//             return { ...user, followers: updatedFollowers };
-//           }
-//           return user;
-//         })
-//       );
-//       return { ...prev, [uid]: !isFollowing };
-//     });
-//   };
-
-//   return (
-//     <VStack w="full" align="start" spacing={4} px={4} py={4}>
-//       <SuggestedHeader />
-//       <Text fontSize="sm" fontWeight="bold" color="gray.500">
-//         Suggested Users
-//       </Text>
-//       {users.map((user) => (
-//         <SuggestedUser
-//           key={user.uid}
-//           userName={user.userName}
-//           avatar={user.profilePicURL}
-//           followersCount={user.followers.length}
-//           fullName={user.fullName}
-//           isFollowing={followState[user.uid]}
-//           onToggleFollow={() => toggleFollow(user.uid)}
-//         />
-//       ))}
-//       <Box fontSize={12} color="gray.500" mt={5}>
-//         Built by Mohammad Ebadi
-//       </Box>
-//     </VStack>
-//   );
-// }
-
-// export default SuggestedUsers;
-
-
 import React, { useEffect, useState } from "react";
 import { VStack, Box, Text } from "@chakra-ui/react";
 import SuggestedUser from "./SuggestedUser.jsx";
@@ -216,7 +117,7 @@ function SuggestedUsers() {
         />
       ))}
       <Box fontSize={12} color="gray.500" mt={5}>
-        Built by Mohammad Ebadi
+        Built by Mohammad Ebadi 2025
       </Box>
     </VStack>
   );
